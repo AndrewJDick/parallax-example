915b1752aa816e687177347b600b70ae55529e25
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-03-09 17:00:14 +0000

    revised version of parallax blocks

--- src/js/app/parallax.js
+++ src/js/app/parallax.js
@@ -12,69 +12,35 @@ export default class Parallax {
       html: document.querySelectorAll('html')[0],
       body: document.querySelectorAll('body')[0],
       parallax: document.querySelectorAll('#parallax')[0],
+      parallax1: document.querySelectorAll('#parallax-1')[0],
       fixed: 'u-fixed',
       active: false,
     }
 
-    this.activate();
+    this.listeners();
   }
 
-  activate() {
+  listeners() {
+
+    var last_known_scroll_position = 0;
+    var ticking = false;
+    var self = this;
     
-    this.active = true;
-    this.elements.parallax.classList.add(this.elements.fixed);
-    this.elements.html.style.overflowY = 'hidden';
+    window.addEventListener('scroll', (e) => { 
 
-    this.setImgHeights();
-  }
+      if (!ticking) {
+        last_known_scroll_position = window.scrollY;
 
-  setImgHeights() {
-    const parallaxRows = this.elements.parallax.getElementsByClassName('c-parallax__row');
-
-    for (let row = 0; row < parallaxRows.length; row++) {
-      
-      // For each row, find the panel quadrant and retrieve it's rendered height;
-      const panel = parallaxRows[row].getElementsByClassName('js-panel-quadrant')[0];
-      const panelQuadrant = parallaxRows[row].getElementsByClassName('c-parallax__panelQuadrant')[0];
-
-      const $rowHeight = $(panel).height();
-
-      // Assign the height amongst each of the row's images
-      const rowImages = parallaxRows[row].getElementsByClassName('c-parallax__imgQuadrant__img');
-      const imgHeight = $rowHeight / rowImages.length;
-
-      for (let img = 0; img < rowImages.length; img++) {
-        rowImages[img].style.height = `${imgHeight}px`;
+        window.requestAnimationFrame(() => {       
+          if (this.elements.parallax.getBoundingClientRect().top <= 0) {
+            this.elements.parallax.classList.add(this.elements.fixed);
+            this.elements.parallax1.style.marginTop = '50vh';
+          }
+          ticking = false;
+        });
       }
-
-      var last_known_scroll_position = 0;
-      var ticking = false;
-      var self = this;
-
-      panelQuadrant.addEventListener('scroll', (e) => { 
-
-        if (!ticking) {
-          last_known_scroll_position = window.scrollY;
-
-          window.requestAnimationFrame(() => {       
-            self.overlapImages(panelQuadrant.scrollTop, rowImages, imgHeight);
-            ticking = false;
-          });
-        }
-
-        ticking = true;
-
-      });
-    }
-  }
-
-  overlapImages(scrollPosition, rowImages, imgHeight) {
-
-    // Reduce the height of the images on scroll, starting with the lowest
-    let value = imgHeight - scrollPosition;
-
-    rowImages[rowImages.length-1].style.height = `${value}px`; 
-
+      ticking = true;
+    });
   }
 }
-  
+
