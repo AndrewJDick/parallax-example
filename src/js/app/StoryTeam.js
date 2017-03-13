7e0a59d7e69765182165a07bdfe9418a394bc1a2
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-03-13 11:54:47 +0000

    blocks successfully overlaying on scroll down

--- src/js/app/StoryTeam.js
+++ src/js/app/StoryTeam.js
@@ -2,7 +2,7 @@ export default class StoryTeam {
   
   constructor() {
     this.elements = {
-      window: window,
+      window,
       html: document.querySelectorAll('html')[0],
       body: document.querySelectorAll('body')[0]
     }
@@ -12,9 +12,14 @@ export default class StoryTeam {
       panels: document.querySelectorAll('.c-team__panel'),
       rows: document.querySelectorAll('.c-team__row'),
       row:  'c-team__row',
-      upper: 'c-team__row--upper',
+      upper: 'c-team__row--upper', 
       lower: 'c-team__row--lower',
-      relative: 'c-team__row--relative'
+      fixed: 'c-team__row--fixed',
+      relative: 'c-team__row--relative',
+      row1: 'c-team__row--1',
+      row2: 'c-team__row--2',
+      row3: 'c-team__row--3',
+      row4: 'c-team__row--4'
     }
 
     this.listeners();
@@ -45,23 +50,33 @@ export default class StoryTeam {
   }
 
 
-  fixedTopRows() {
+  fixedTopRows() { 
 
     const rowHeight = this.team.container.offsetHeight / this.team.rows.length; 
+    const containerTop = this.team.container.getBoundingClientRect().top;
     
+    //console.log(containerTop);
+
     for (let r = 0; r < this.team.rows.length; r++) {
 
-      const row = this.team.rows[r];
-      const rowTop = row.getBoundingClientRect().top;
+      const row1 = this.team.rows[r];
+      const row1Top = row1.getBoundingClientRect().top;
 
-      if ( -rowHeight < rowTop && rowTop < 0 ) {
 
-        row.classList.add(this.team.upper);
-        row.nextSibling.classList.add(this.team.lower);
+      if ( row1Top < 0 ) {
+
+        const row2 = row1.nextSibling;
+        const row3 = row2.nextSibling;
+        const row4 = row3.nextSibling;
+
+        row1.classList.add(this.team.fixed, this.team.upper);
+        row2.classList.add(this.team.fixed, this.team.lower);
+
+        row1.classList.remove(this.team.relative);
+        row2.classList.remove(this.team.relative);
         
-        row.classList.remove(this.team.relative);
-        row.nextSibling.classList.remove(this.team.relative);
-
+        row3.classList.add(this.team.relative);
+        row4.classList.add(this.team.relative);
       }
     }
   }
