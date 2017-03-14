c2d1a92ce00a7770ef84a8ec02fe12f0b1e0485c
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-03-14 13:48:13 +0000

    sliding grid now works both ways. needs refactored

--- src/js/app/StoryTeam.js
+++ src/js/app/StoryTeam.js
@@ -27,6 +27,7 @@ export default class StoryTeam {
   }
 
 
+  // TODO: Add Debounce to scroll event
   listeners() {
 
     this.elements.window.addEventListener('scroll', (e) => { 
@@ -68,138 +69,86 @@ export default class StoryTeam {
 
   gridOverlap() { 
 
-    // (this.scroll.direction) ? console.log('scrollDown') : console.log('scrollUp');
-
     this.team.quadrants.forEach( (quadrant, index) => {
 
       if (this.isVisible(quadrant) && this.scroll.direction) {
+
+        quadrant.firstChild.classList.add(this.team.upper);
+        quadrant.lastChild.classList.add(this.team.lower);
+
+        quadrant.firstChild.classList.remove(this.team.relative);
+        quadrant.lastChild.classList.remove(this.team.relative);
+
+        quadrant.firstChild.style.zIndex = 5;
+        quadrant.lastChild.style.zIndex = 15;
+
+        if (quadrant.previousSibling) {
+
+          quadrant.previousSibling.firstChild.classList.remove(this.team.upper);
+          quadrant.previousSibling.lastChild.classList.remove(this.team.lower);
+
+          quadrant.previousSibling.firstChild.classList.add(this.team.relative);
+          quadrant.previousSibling.lastChild.classList.add(this.team.relative);
+
+          quadrant.previousSibling.firstChild.style.zIndex = 10;
+          quadrant.previousSibling.lastChild.style.zIndex = 20;
+        } 
         
         if (quadrant.nextSibling) {
-          quadrant.firstChild.classList.add(this.team.upper);
-          quadrant.lastChild.classList.add(this.team.lower);
-
+    
           quadrant.firstChild.classList.remove(this.team.relative);
           quadrant.lastChild.classList.remove(this.team.relative);
 
           quadrant.nextSibling.firstChild.classList.add(this.team.relative);
           quadrant.nextSibling.lastChild.classList.add(this.team.relative);
+
+          quadrant.nextSibling.firstChild.style.zIndex = 10;
+          quadrant.nextSibling.lastChild.style.zIndex = 20;
         } else {
           this.clearClasses();
         }
       }
 
+      if (this.isVisible(quadrant) && !this.scroll.direction) {
+        
+        quadrant.firstChild.classList.add(this.team.upper);
+        quadrant.lastChild.classList.add(this.team.lower);
+
+        quadrant.firstChild.classList.remove(this.team.relative);
+        quadrant.lastChild.classList.remove(this.team.relative);
+
+        quadrant.firstChild.style.zIndex = 15;
+        quadrant.lastChild.style.zIndex = 5;
+
+        if (quadrant.nextSibling) {
+
+          quadrant.nextSibling.firstChild.classList.remove(this.team.upper);
+          quadrant.nextSibling.lastChild.classList.remove(this.team.lower);
+
+          quadrant.nextSibling.firstChild.classList.add(this.team.relative);
+          quadrant.nextSibling.lastChild.classList.add(this.team.relative);
+
+          quadrant.nextSibling.firstChild.style.zIndex = 20;
+          quadrant.nextSibling.lastChild.style.zIndex = 10;
+        }
+
+        if (quadrant.previousSibling) {
+
+          quadrant.previousSibling.firstChild.classList.remove(this.team.upper);
+          quadrant.previousSibling.lastChild.classList.remove(this.team.lower);
+
+          quadrant.previousSibling.firstChild.classList.add(this.team.relative);
+          quadrant.previousSibling.lastChild.classList.add(this.team.relative);
+
+          quadrant.previousSibling.firstChild.style.zIndex = 20;
+          quadrant.previousSibling.lastChild.style.zIndex = 10;
+        } else {
+          this.clearClasses();
+        }
+
+      }
+
     });
-
-      // this.team.rows.forEach( (row, index) => {
-      //   const lastRow = this.team.container.lastChild.getBoundingClientRect().bottom;
-      //   const viewportHeight = this.elements.window.innerHeight;
-      //   const row1 = row;
-
-      //   let row2, row3, row4, rowNeg1, rowNeg2;
-
-      // Fix a row to the browser window once it hits the top of the page
-      // if ( row1.getBoundingClientRect().top < 0 && lastRow > window.innerHeight) {
-
-      //   if (row1.previousSibling) {
-      //     rowNeg1 = row1.previousSibling;
-      //     rowNeg2 = rowNeg1.previousSibling;
-
-      //     rowNeg1.classList.add(this.team.relative);
-      //     rowNeg2.classList.add(this.team.relative);
-
-      //     rowNeg1.classList.remove(this.team.lower);
-      //     rowNeg2.classList.remove(this.team.upper);
-      //   }
-
-      //   if (row1.nextSibling) {
-      //     row2 = row1.nextSibling;
-
-      //     row1.classList.add(this.team.upper);
-      //     row2.classList.add(this.team.lower);
-
-      //     row1.classList.remove(this.team.relative);
-      //     row2.classList.remove(this.team.relative);
-      //   }
-
-      //   if (row2.nextSibling) {
-      //     row3 = row2.nextSibling;
-      //     row4 = row3.nextSibling;
-
-      //     row3.classList.add(this.team.relative);
-      //     row4.classList.add(this.team.relative);
-      //   }
-      // } 
-
-      // if (lastRow < window.innerHeight) {
-      //   this.clearClasses();
-      // }
-      // });
   }
-
- 
-
-  scrollUp() {
-    
-    // const reverseRows = [].slice.call(this.team.rows).reverse();
-
-    // reverseRows.forEach( (row, index) => {
-    //   const lastRow = this.team.container.lastChild.getBoundingClientRect().bottom;
-    //   const row4 = row;
-
-    //   let row3, row2, row1;
-
-    //   row.classList.add(`c-team__row--${index + 1}`);
-
-
-    //   if ( row4.getBoundingClientRect().bottom > window.innerHeight) {
-    //     console.log('bang');
-
-    //     if (row4.previousSibling) {
-    //       row3 = row4.previousSibling;
-
-    //       row3.classList.add(this.team.upper);
-    //       row4.classList.add(this.team.lower);
-
-    //       row3.classList.remove(this.team.relative, 'final');
-    //       row4.classList.remove(this.team.relative, 'final');
-    //     }
-
-    //     if (row3.previousSibling) {
-    //       row2 = row3.previousSibling;
-    //       row1 = row2.previousSibling;
-
-    //       row2.classList.add(this.team.relative, 'final');
-    //       row1.classList.add(this.team.relative, 'final');
-    //     }
-    //   }
-
-
-    // });
-  }
-
-  
-
-  // injectPlaceholder(row) {
-
-  //   const element = this.team.container;
-  //   const fragment = document.createDocumentFragment();
-
-  //   var div = document.createElement('div');
-  //   div.style.height = '50vh';
-  //   div.setAttribute('class', 'js-placeholder');
-  //   fragment.appendChild(div);
-
-  //   element.insertBefore(fragment, row);
-
-  // }
-
-  // removePlaceholders() {
-
-  //   const placeholders = document.getElementsByClassName('js-placeholder');
-    
-  //   for (let p = 0; p < placeholders.length; p++) {
-  //     placeholders[p].parentNode.removeChild(placeholders[p]);
-  //   }
-  // }
 }
 
