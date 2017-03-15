8c0cecef136d0c6dcd4e287eef03e533e7866a3b
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-03-15 12:44:23 +0000

    refactored global variables

--- src/js/app/StoryTeam.js
+++ src/js/app/StoryTeam.js
@@ -1,20 +1,6 @@
 export default class StoryTeam {
   
   constructor() {
-    this.elements = {
-      html: document.querySelectorAll('html')[0],
-      body: document.querySelectorAll('body')[0]
-    }
-
-    this.team = {
-      container: document.querySelectorAll('#story-team')[0],
-      quadrants: document.querySelectorAll('.c-team__quadrant'),
-      rows: document.querySelectorAll('.c-team__row'),
-      row:  'c-team__row',
-      upper: 'c-team__row--upper', 
-      lower: 'c-team__row--lower',
-      relative: 'c-team__row--relative'
-    }
 
     this.scroll = {
       previous: 0,
@@ -23,24 +9,9 @@ export default class StoryTeam {
       ticking: false
     }
 
-    this.init();
-  }
-
-  init() {
-    this.team.quadrants.forEach( (quadrant, index) => {
-
-      quadrant.setAttribute('pTop', 0);
-      quadrant.setAttribute('cTop', 0);
-
-      quadrant.setAttribute('pBot', 0);
-      quadrant.setAttribute('cBot', 0);
-    
-    });
-
     this.listeners();
   }
 
-  // TODO: Add Debounce to scroll event
   listeners() {
     window.addEventListener('scroll', () => this.onScroll(), false);
   }
@@ -62,42 +33,57 @@ export default class StoryTeam {
 
   requestTick() {
     if(!this.scroll.ticking) {
-      window.requestAnimationFrame(() => this.gridOverlap());
+      window.requestAnimationFrame(() => this.update());
     }
     this.scroll.ticking = true;
   }
 
-  gridOverlap() { 
+  update() { 
 
     this.scroll.ticking = false;
 
-    //if (this.team.container.getBoundingClientRect().top <= 0) {
+    const peopleContainer = document.querySelectorAll('#story-team')[0];
 
-      this.team.quadrants.forEach( (quadrant, index) => {
+    if (peopleContainer.getBoundingClientRect().top <= 0 || peopleContainer.getBoundingClientRect().bottom > 0 ) {
 
-        const currentTop = quadrant.getBoundingClientRect().top;
-        const currentBot = quadrant.getBoundingClientRect().bottom;
-        const vpHeight   = window.innerHeight;
+      const viewportHeight = window.innerHeight;
 
-        // User is scrolling down and the quadrant crosses the top of the viewport
-        if (currentTop <= 0 && currentBot > 0 && this.scroll.direction) {
+      const teamElements = {
+        quadrants: document.querySelectorAll('.c-team__quadrant'),
+        rows: document.querySelectorAll('.c-team__row')
+      };
 
-          quadrant.firstChild.classList.add(this.team.upper);
-          quadrant.lastChild.classList.add(this.team.lower);
+      const teamClasses = { 
+        row:  'c-team__row',
+        upper: 'c-team__row--upper', 
+        lower: 'c-team__row--lower',
+        relative: 'c-team__row--relative'
+      };
 
-          quadrant.firstChild.classList.remove(this.team.relative);
-          quadrant.lastChild.classList.remove(this.team.relative);
+      teamElements.quadrants.forEach( (quadrant) => {
+
+        const quadrantTop = quadrant.getBoundingClientRect().top;
+        const quadrantBot = quadrant.getBoundingClientRect().bottom;
+        
+        // Scroll down
+        if (quadrantTop <= 0 && quadrantBot > 0 && this.scroll.direction) {
+
+          quadrant.firstChild.classList.add(teamClasses.upper);
+          quadrant.lastChild.classList.add(teamClasses.lower);
+
+          quadrant.firstChild.classList.remove(teamClasses.relative);
+          quadrant.lastChild.classList.remove(teamClasses.relative);
 
           quadrant.firstChild.style.zIndex = 5;
           quadrant.lastChild.style.zIndex = 15;
 
           if (quadrant.previousSibling) {
 
-            quadrant.previousSibling.firstChild.classList.remove(this.team.upper);
-            quadrant.previousSibling.lastChild.classList.remove(this.team.lower);
+            quadrant.previousSibling.firstChild.classList.remove(teamClasses.upper);
+            quadrant.previousSibling.lastChild.classList.remove(teamClasses.lower);
 
-            quadrant.previousSibling.firstChild.classList.add(this.team.relative);
-            quadrant.previousSibling.lastChild.classList.add(this.team.relative);
+            quadrant.previousSibling.firstChild.classList.add(teamClasses.relative);
+            quadrant.previousSibling.lastChild.classList.add(teamClasses.relative);
 
             quadrant.previousSibling.firstChild.style.zIndex = 10;
             quadrant.previousSibling.lastChild.style.zIndex = 20;
@@ -105,38 +91,38 @@ export default class StoryTeam {
           
           if (quadrant.nextSibling) {
       
-            quadrant.firstChild.classList.remove(this.team.relative);
-            quadrant.lastChild.classList.remove(this.team.relative);
+            quadrant.firstChild.classList.remove(teamClasses.relative);
+            quadrant.lastChild.classList.remove(teamClasses.relative);
 
-            quadrant.nextSibling.firstChild.classList.add(this.team.relative);
-            quadrant.nextSibling.lastChild.classList.add(this.team.relative);
+            quadrant.nextSibling.firstChild.classList.add(teamClasses.relative);
+            quadrant.nextSibling.lastChild.classList.add(teamClasses.relative);
 
             quadrant.nextSibling.firstChild.style.zIndex = 10;
             quadrant.nextSibling.lastChild.style.zIndex = 20;
           } else {
-            this.clearClasses();
+            this.clearClasses(teamElements.rows, teamClasses);
           }
         }
 
-        // User is scrolling up and the quadrant crosses the bottom of the viewport
-        if (currentBot >= vpHeight && currentTop < vpHeight && !this.scroll.direction) {
+        // Scroll Up
+        if (quadrantBot >= viewportHeight && quadrantTop < viewportHeight && !this.scroll.direction) {
 
-          quadrant.firstChild.classList.add(this.team.upper);
-          quadrant.lastChild.classList.add(this.team.lower);
+          quadrant.firstChild.classList.add(teamClasses.upper);
+          quadrant.lastChild.classList.add(teamClasses.lower);
 
-          quadrant.firstChild.classList.remove(this.team.relative);
-          quadrant.lastChild.classList.remove(this.team.relative);
+          quadrant.firstChild.classList.remove(teamClasses.relative);
+          quadrant.lastChild.classList.remove(teamClasses.relative);
 
           quadrant.firstChild.style.zIndex = 15;
           quadrant.lastChild.style.zIndex = 5;
 
           if (quadrant.nextSibling) {
 
-            quadrant.nextSibling.firstChild.classList.remove(this.team.upper);
-            quadrant.nextSibling.lastChild.classList.remove(this.team.lower);
+            quadrant.nextSibling.firstChild.classList.remove(teamClasses.upper);
+            quadrant.nextSibling.lastChild.classList.remove(teamClasses.lower);
 
-            quadrant.nextSibling.firstChild.classList.add(this.team.relative);
-            quadrant.nextSibling.lastChild.classList.add(this.team.relative);
+            quadrant.nextSibling.firstChild.classList.add(teamClasses.relative);
+            quadrant.nextSibling.lastChild.classList.add(teamClasses.relative);
 
             quadrant.nextSibling.firstChild.style.zIndex = 20;
             quadrant.nextSibling.lastChild.style.zIndex = 10;
@@ -144,25 +130,28 @@ export default class StoryTeam {
 
           if (quadrant.previousSibling) {
 
-            quadrant.previousSibling.firstChild.classList.remove(this.team.upper);
-            quadrant.previousSibling.lastChild.classList.remove(this.team.lower);
+            quadrant.previousSibling.firstChild.classList.remove(teamClasses.upper);
+            quadrant.previousSibling.lastChild.classList.remove(teamClasses.lower);
 
-            quadrant.previousSibling.firstChild.classList.add(this.team.relative);
-            quadrant.previousSibling.lastChild.classList.add(this.team.relative);
+            quadrant.previousSibling.firstChild.classList.add(teamClasses.relative);
+            quadrant.previousSibling.lastChild.classList.add(teamClasses.relative);
 
             quadrant.previousSibling.firstChild.style.zIndex = 20;
             quadrant.previousSibling.lastChild.style.zIndex = 10;
           } else {
-            this.clearClasses();
+            this.clearClasses(teamElements.rows, teamClasses);
           }
         }
       });
-    //}
+    }
   }
 
-  clearClasses() {
-    this.team.rows.forEach((row, index) => {
-      row.setAttribute('class', `${this.team.row} ${this.team.row}--${index + 1}`);
+  clearClasses(rows, classes) {
+
+    rows.forEach((row, index) => {
+      row.classList.remove(classes.upper);
+      row.classList.remove(classes.lower);
+      row.classList.remove(classes.relative);
     });
   }
 }
